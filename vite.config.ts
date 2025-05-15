b2d6646e8afd19dee91d60297bae06c7b4a438eb
oniafk <manufelipearias@gmail.com>
2025-05-15 11:43:31 +1000

    trying deploy again ...

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,7 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
+  esbuild: {
+    target: "esnext", // or 'edge89', 'chrome89' — something that supports top-level await
+  },
 });
