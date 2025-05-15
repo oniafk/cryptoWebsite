68ca7affc193b761fe09f725612bb0d7084ddc4e
oniafk <manufelipearias@gmail.com>
2025-05-15 13:38:26 +1000

    added topLevelawait for rapier

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
+
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [tailwindcss(), wasm(), topLevelAwait()],
   esbuild: {
     target: "esnext", // or 'edge89', 'chrome89' — something that supports top-level await
   },
